9f937412be9c949cc8b2084191429d4918d06898
govardhan <govardhanraju92@gmail.com>
2018-03-06 12:13:36 +0800

    UI changes..

---
+++ src/Test/Test.js
@@ -0,0 +1,8 @@
+describe("the number validate",function () {
+
+    it('should return true when value is added', function () {
+        var node =
+        except(increases(value)).toBe(true);
+    });
+
+})
